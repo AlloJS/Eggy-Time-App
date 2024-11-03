a31393591a4fd483445fcd74068e2a183359d84c
AlloJS <eventorganization1@gmail.com>
2024-11-03 11:57:07 +0100

    Initial Commit

---
+++ Eggy Timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Eggy Timer
+//
+//  Created by Angelo Bellanca on 03/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Eggy TimerTests/Eggy_TimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Eggy_TimerTests.swift
+//  Eggy TimerTests
+//
+//  Created by Angelo Bellanca on 03/11/24.
+//
+
+import Testing
+@testable import Eggy_Timer
+
+struct Eggy_TimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
